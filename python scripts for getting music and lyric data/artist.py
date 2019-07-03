e8f5f965da6d1749a3c06b0e064ab02e3b854102
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-07-03 14:08:14 -0300

    atualizando os seguidores para os novos artistas alvo

--- python scripts for getting music and lyric data/artist.py
+++ python scripts for getting music and lyric data/artist.py
@@ -6,10 +6,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 my_id = '655e62800e724511b9a6f74b1bfa635f'
 secret_key = '9e1f1feb45874771b24d1d10d19e87bd'
 
-artist_names = ["BTS", "AOA", "SHINee", "Girls' Generation", "Red Velvet", "Wanna One", "iKON", "GFRIEND", "BIGBANG"]
-#BTS, "BLACKPINK", "Twice", "EXO", "Got7", "Red Velvet", "Girls' Generation", "iKON", "MONSTA X", "SHINee"
-
-# "Shawn Mendes", "Billie Eilish", "Ariana Grande", "Khalid", "Halsey", "Rihanna", "Bruno Mars", "Beyonce", "Lady Gaga", "Katy Perry"
+artist_names = ["BTS", "BLACKPINK", "Twice", "EXO", "Got7", "Red Velvet", "Girls' Generation", "iKON", "MONSTA X", "BIGBANG", "Shawn Mendes", "Billie Eilish", "Ariana Grande", "Khalid", "Halsey", "Rihanna", "Bruno Mars", "Beyonce", "Lady Gaga", "Katy Perry"]
 client_credentials_manager = SpotifyClientCredentials(client_id = my_id, client_secret = secret_key)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
@@ -25,4 +22,4 @@ data = pd.DataFrame(artists_list, columns = ["name", "followers"])
 
 print(data)
 
-data.to_csv('../data/name_and_followers_kpop.csv')
+data.to_csv('../data/artists_name_and_followers.csv')
