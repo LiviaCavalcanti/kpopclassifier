5c4679f21654ceb61c85af94d6f168e6e112d8cc
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-07-03 14:02:47 -0300

    generalizando script de coleta de dados

--- python scripts for getting music and lyric data/getting_tracks.py
+++ python scripts for getting music and lyric data/getting_tracks.py
@@ -6,52 +6,53 @@ from spotipy.oauth2 import SpotifyClientCredentials
 my_id = '655e62800e724511b9a6f74b1bfa635f'
 secret_key = '9e1f1feb45874771b24d1d10d19e87bd'
 
-artist_names = ["BTS", "AOA", "SHINee", "Girls' Generation", "Red Velvet", "Wanna One", "iKON", "GFRIEND", "BIGBANG"]
+kartist_names = ["BTS", "BLACKPINK", "Twice", "EXO", "Got7", "Red Velvet", "Girls' Generation", "iKON", "MONSTA X", "BIGBANG"]
 #BTS, "BLACKPINK", "Twice", "EXO", "Got7", "Red Velvet", "Girls' Generation", "iKON", "MONSTA X", "SHINee"
 
-# "Shawn Mendes", "Billie Eilish", "Ariana Grande", "Khalid", "Halsey", "Rihanna", "Bruno Mars", "Beyonce", "Lady Gaga", "Katy Perry"
+aartist_names = ["Shawn Mendes", "Billie Eilish", "Ariana Grande", "Khalid", "Halsey", "Rihanna", "Bruno Mars", "Beyonce", "Lady Gaga", "Katy Perry"]
 client_credentials_manager = SpotifyClientCredentials(client_id = my_id, client_secret = secret_key)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-artists_list = []
+
 feature_list = []
-isKPop = True
-for a in artist_names:
-    results = spotify.search(q='artist:' + a, type='artist')
+KPop = True
+artists_list = {'kpop':(kartist_names, KPop), 'apop':(aartist_names, not KPop)}
 
-    top10_tracks = spotify.artist_top_tracks(results['artists']['items'][0]['uri'])['tracks']
+for artists in artists_list.items():
+    artist_names = artists[1][0]
+    isKPop = artists[1][1]
+    for a_name in artist_names:
+        results = spotify.search(q='artist:' + a_name, type='artist')
 
-    for each_track in top10_tracks:
-        track_features = spotify.audio_features(each_track['uri'])[0]
-        feature_list.append([track_features['danceability'], 
-                            track_features['energy'], 
-                            track_features['key'], 
-                            track_features['speechiness'],
-                            track_features['acousticness'], 
-                            track_features['instrumentalness'], 
-                            track_features['liveness'], 
-                            track_features['valence'],
-                            track_features['tempo'], 
-                            track_features['duration_ms'],
-                            track_features['time_signature'], 
-                            each_track['artists'][0]['name'], 
-                            each_track['album']['release_date'], 
-                            each_track['name'], 
-                            each_track['explicit'], 
-                            each_track['album']['name'], 
-                            each_track['album']['total_tracks'], 
-                            len(each_track['artists']), 
-                            each_track['popularity'], 
-                            isKPop])
+        top10_tracks = spotify.artist_top_tracks(results['artists']['items'][0]['uri'])['tracks']
 
-    data = pd.DataFrame(feature_list, columns = ['danceability','energy','key','speechiness','acousticness','instrumentalness','liveness','valence','tempo','duration_ms','time_signature', 'artist_name', 'release_date', 'song_name', 'isExplicit', 'albumName', 'albumTotalTracks', 'totalArtists', 'popularity', 'isKPop'])
-    print(data) 
-    break
-    # items = results['artists']['items']
-    # artist = items[0]
-    # artists_list.append([artist["name"], artist["followers"]["total"]])
+        for each_track in top10_tracks:
+            track_features = spotify.audio_features(each_track['uri'])[0]
+            feature_list.append([track_features['danceability'], 
+                                track_features['energy'], 
+                                track_features['key'], 
+                                track_features['speechiness'],
+                                track_features['acousticness'], 
+                                track_features['instrumentalness'], 
+                                track_features['liveness'], 
+                                track_features['valence'],
+                                track_features['tempo'], 
+                                track_features['duration_ms'],
+                                track_features['time_signature'], 
+                                a_name, 
+                                each_track['album']['release_date'], 
+                                each_track['name'], 
+                                each_track['explicit'], 
+                                each_track['album']['name'], 
+                                each_track['album']['total_tracks'], 
+                                len(each_track['artists']), 
+                                each_track['popularity'], 
+                                isKPop])
 
-# data = pd.DataFrame(artists_list, columns = ["name", "followers"])
+
+data = pd.DataFrame(feature_list, columns = ['danceability','energy','key','speechiness','acousticness','instrumentalness','liveness','valence','tempo','duration_ms','time_signature', 'artist_name', 'release_date', 'song_name', 'isExplicit', 'albumName', 'albumTotalTracks', 'totalArtists', 'popularity', 'isKPop'])
+data.to_csv('../data/new_song_features.csv')
+
 
 # 11834886
 #  {'tracks': 
